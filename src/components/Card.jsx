16b17a14823216597df49875a1aac8699c170ad3
Darko Ivkov <bVNyDUKE@protonmail.com>
2022-01-02 12:41:44 +0100

    fix some accessibility report issues

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,10 +4,10 @@ export default function Card({ name, flag, capital, population, region }){
   return(
     <article className='bg-white text-blue-955 dark:bg-blue-950 dark:text-white shadow-md rounded-md overflow-hidden hover:cursor-pointer hover:shadow-xl'>
 
-      <img className='object-cover md:w-[365px] md:h-[256px] float-left' src={flag} />
+      <img className='object-cover md:w-[365px] md:h-[256px] float-left' src={flag} alt={name} />
 
       <div className='h-1/2 px-5 pb-12 pt-5 clear-left'>
-        <p className='font-bold text-lg mb-3'>{name}</p>
+        <h2 className='font-bold text-lg mb-3'>{name}</h2>
         <div className='text-sm'>
           <p><span className='font-semibold'>Population: </span> {population.toLocaleString()}</p>
           <p><span className='font-semibold'>Region: </span> {region} </p>
