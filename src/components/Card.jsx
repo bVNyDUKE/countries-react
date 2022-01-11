abfe1e94b537a711b226c4327e6b7f952f043fb5
Darko Ivkov <bVNyDUKE@protonmail.com>
2022-01-11 23:19:35 +0100

    remove flag size specifying

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,7 +4,7 @@ export default function Card({ name, flag, capital, population, region }){
   return(
     <article className='bg-white text-blue-955 dark:bg-blue-950 dark:text-white shadow-md rounded-md overflow-hidden hover:cursor-pointer hover:shadow-xl transition-colors ease-in-out duration-1000'>
 
-      <img className='object-cover md:w-[365px] md:h-[256px] float-left' src={flag} alt={name} />
+      <img className='object-cover' src={flag} alt={name} />
 
       <div className='h-1/2 px-5 pb-12 pt-5 clear-left'>
         <h2 className='font-bold text-lg mb-3'>{name}</h2>
